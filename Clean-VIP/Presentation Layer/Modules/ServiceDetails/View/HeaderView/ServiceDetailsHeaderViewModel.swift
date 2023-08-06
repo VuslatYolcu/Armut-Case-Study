f77e839359db8825d15da3deec971611aa8941e6
VuslatYolcu <ylcvalt@gmail.com>
2023-08-06 17:32:05 +0300

    Add header view again

---
+++ Clean-VIP/Presentation Layer/Modules/ServiceDetails/View/HeaderView/ServiceDetailsHeaderViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  ServiceDetailsHeaderViewModel.swift
+//  Clean-VIP
+//
+//  Created by Vuslat Yolcu on 6.08.2023.
+//  Copyright © 2023 Zafar. All rights reserved.
+//
+
+struct ServiceDetailsHeaderViewModel {
+    let title: String
+    let imageURL: String?
+}
